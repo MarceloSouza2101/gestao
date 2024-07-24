305fdc93cf350cc919360a361142a945db209bed
mssantos <marsantos@apoioprodesp.sp.gov.br>
2024-07-23 22:39:48 -0300

    Feature:CRUD-fornecedor-produtos

---
+++ src/main/java/com/marcelo/gestao/configuration/SpringDocConfig.java
@@ -0,0 +1,13 @@
+package com.marcelo.gestao.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Generated;
+
+@Generated
+@Configuration
+public class SpringDocConfig {
+
+	// http:localhost:8080/gestao/swagger-ui/index.html
+
+}
